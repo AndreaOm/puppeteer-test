80a18cf119230bd074fb3cb189892b93188f0342
z0z <472354643@qq.com>
2017-09-06 12:58:14 +0800

    #1

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const createRenderer = require('./renderer');
+
+const port = process.env.PORT || 3001;
+
+let renderer = null;
+
+const app = express();
+
+
+app.disable('x-powered-by');
+
+
+app.use(async (req, res, next) => {
+  if (!req.query.url) {
+    return res.status(400).send('For eaxample, ?url=http://github.com');
+  }
+
+  try {
+
+    const html = await renderer.render(req.query.url);
+    res.status(200).send(html);
+
+  } catch (e) {
+    next(e);
+  }
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Oops, An expected error seems to have occurred.');
+});
+
+
+createRenderer().then((createdRender) => {
+  renderer = createdRender;
+  console.info('Initialized renderer.');
+
+  app.listen(port, () => {
+    console.info(`Listen port on ${port}.`);
+  });
+}).catch((e) => {
+  console.error('Fail to initialze renderer.', e);
+});
---
+++ renderer.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+class Renderer {
+  constructor(browser) {
+    this.browser = browser;
+  }
+
+  async createPage(url) {
+    const page = await this.browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle' });
+    return page;
+  }
+
+  async render(url) {
+    const page = await this.createPage(url);
+    const html = await page.evaluate(() => {
+      return document.documentElement.outerHTML;
+    });
+
+    await page.close();
+    return html;
+  }
+
+}
+
+async function create() {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  return new Renderer(browser);
+}
+
+module.exports = create;
